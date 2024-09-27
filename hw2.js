ffadf201cd2628cbf54ee6ecd148cd8bd706e302
KovalevskaOlga <146865293+KovalevskaOlga@users.noreply.github.com>
2024-09-27 21:55:09 +0300

    hw2

---
+++ hw2.js
@@ -0,0 +1,51 @@
+//TASK1
+const celsius = 32;
+const fahrenheit = (celsius * 9 / 5) + 32;
+console.log(fahrenheit + "°F");
+
+//TASK2
+const daysInMonth = 30;
+const hourInMonth = (daysInMonth * 24);
+const minutesInMonth = (hourInMonth * 60);
+console.log(hourInMonth);
+console.log(minutesInMonth);
+
+//TASK3
+let health = 100;
+let energy = 80;
+health -= 15;
+energy -= 20;
+console.log("здоров'я " + health);
+console.log("енергія " + energy);
+
+//TASK4
+const buy = 500;
+const discover = 0.1;
+const sale = buy * (1 - discover);
+console.log(sale)
+
+//TASK5
+const number = 1.76;
+console.log(Math.floor(number));
+
+//TASK6
+const number2 = 0.23746;
+console.log(Number.parseFloat(number2))
+
+//TASK7
+const number3 = 25.53;
+console.log(Number.parseInt(number3));
+
+//TASK8 
+const number4 = 4;
+console.log(Math.sqrt(number4));
+
+//TASK9
+const number5 = 17;
+const numberExchange = number5.toString();
+console.log(numberExchange);
+const number6 = "17.9876";
+const numberTranslate = Number.parseInt(number2);
+console.log(numberTranslate);
+
+
